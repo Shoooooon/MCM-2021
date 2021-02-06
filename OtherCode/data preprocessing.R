7d7806784a9b7c92160fd2e4250f4faf9bfc3abe
bohanwu0424 <bohanwu@icloud.com>
2021-02-06 15:37:26 -0600

    data preprocesing

--- OtherCode/data preprocessing.R
+++ OtherCode/data preprocessing.R
@@ -20,7 +20,7 @@ for(name in filenames[2:length(filenames)]){
 }
 
 #-----------------------------------------------------------------
-#Make csv file: 
+#Make csv file:
 #1) Select Victoria
 dat_victoria = dat_all %>% filter((latitude < -36&latitude >= -39& longitude <142& longitude >= 141)|
                                     (latitude < -35&latitude >= -39& longitude <144& longitude >= 142)|
@@ -35,44 +35,3 @@ dat_victoria_wildfire = dat_victoria%>% filter(type == 0)%>%
 
 #Write processed csv file
 #write.csv(dat_victoria_wildfire,file = "wildfire data 2012-2021.csv")
-dat_victoria_wildfire2019 = dat_victoria_wildfire %>% select(latitude,longitude ,bright_ti4,acq_date) %>%
-  filter(year(acq_date) == 2019)
-
-
-leaflet(data = dat_victoria_wildfire2019) %>%
-  addTiles() %>%
-  addCircleMarkers(lat = ~latitude, lng = ~longitude, stroke = FALSE, fillOpacity = 0.6) #%>%
-  #addLegend(position = "bottomleft", pal = pal, values = ~pop)
-
-str(dat_victoria_wildfire2019)
-
-ggplot(dat_victoria_wildfire2019, aes(longitude, latitude, color = bright_ti4)) +
-  geom_point(alpha = 0.7, show.legend = FALSE) +
-  scale_size(range = c(2, 12)) +
-  # Here comes the gganimate specific bits
-  labs(title = 'Year: {frame_time}', x = 'GDP per capita', y = 'life expectancy') +
-  transition_time(acq_date) +
-  ease_aes('linear')
-
-world <- map_data("world")
-Australia <- world %>% filter(region == "Australia")
-summary(australia$subregion %>% as.factor())
-
-p = ggplot(dat_victoria_wildfire2019,
-       aes(x = longitude, y = latitude,color = bright_ti4))+
-  geom_map(data = Australia,
-           map = Australia,
-           aes(long,lat,map_id = region),
-           color = '#333300', fill = '#663300',
-           # lighter background for better visibility
-           alpha = 0.5) +
-  geom_point(size = 2.5) +
-  geom_point(size = bright_ti4)+
-  # limit coordinates to relevant range
-  coord_quickmap(x = c(140, 155), y = c(-30, -45)) +
-  transition_states(states = acq_date)
-
-gganimate::animate(
-  p + enter_fade() + exit_fly(y_loc = 1),
-  renderer = av_renderer()
-)
